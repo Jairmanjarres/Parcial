9a70226509baca1576be4ba24611f82fd905025e
Jair Manjarres <jhayr.m.r@gmail.com>
2021-09-25 00:31:22 -0500

    jmanjarresr@unicesar.edu.co

--- recursos/js/funciones.js
+++ recursos/js/funciones.js
@@ -3,13 +3,13 @@ var db = openDatabase("itemDB", "1.0", "itemDB", 65535)
 
 function limpiar() {
     document.getElementById("item").value = "";
-    document.getElementById("precio").value = "";
+    document.getElementById("contacto").value = "";
 }
 function eliminarRegistro() {
     $(document).one('click', 'button[type="button"]', function (event) {
         let id = this.id;
         var lista = [];
-        $("#listaProductos").each(function () {
+        $("#listaCliente").each(function () {
             var celdas = $(this).find('tr.Reg_' + id);
             celdas.each(function () {
                 var registro = $(this).find('span.mid');
@@ -19,7 +19,7 @@ function eliminarRegistro() {
             });
         });
         nuevoId = lista[0].substr(1);
-        //alert(nuevoId);
+        
         db.transaction(function (transaction) {
             var sql = "borrar=" + nuevoId + ";"
             transaction.executeSql(sql, undefined, function () {
@@ -29,4 +29,22 @@ function eliminarRegistro() {
             })
         })
     });
+}
+function editar(){
+    $(document).one('click','button[type="button"]', function(event){
+    let id=this.id;
+    var lista=[];
+    $("#listaProductos").each(function(){
+        var celdas=$(this).find('tr.Reg_'+id);
+        celdas.each(function(){
+            var registro=$(this).find('span');
+            registro.each(function(){
+                lista.push($(this).html())
+            });
+        });
+    });
+    document.getElementById("item").value=lista[1];
+    document.getElementById("sueldo").value=lista[2].slice(0,-5);
+    nuevoId=lista[0].substr(1);
+})
 }
