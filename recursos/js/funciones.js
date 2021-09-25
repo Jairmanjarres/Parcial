a2eb0865953e254aa7340ab0d9fe12298b497336
Jair Manjarres <jhayr.m.r@gmail.com>
2021-09-24 23:24:27 -0500

    jmanjarresr@unicesar.edu.co

--- recursos/js/funciones.js
+++ recursos/js/funciones.js
@@ -5,3 +5,16 @@ function limpiar(){
 	document.getElementById("item").value="";
 	document.getElementById("precio").value="";
  }
+ function eliminarRegistro(){
+	$(document).one('click','button[type="button"]', function(event){
+		let id=this.id;
+		var lista=[];
+		$("#listaProductos").each(function(){
+			var celdas=$(this).find('tr.Reg_'+id);
+			celdas.each(function(){
+				var registro=$(this).find('span.mid');
+				registro.each(function(){
+					lista.push($(this).html())
+				});
+			});
+		});
