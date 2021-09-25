3e696139e9188ccb48cfe88bf29d372b38ca8274
Jair Manjarres <jhayr.m.r@gmail.com>
2021-09-24 23:28:39 -0500

    jmanjarresr@jmanjarresr.edu.co

--- recursos/js/funciones.js
+++ recursos/js/funciones.js
@@ -1,20 +1,32 @@
 var nuevoId;
-var db=openDatabase("itemDB","1.0","itemDB", 65535)
+var db = openDatabase("itemDB", "1.0", "itemDB", 65535)
 
-function limpiar(){
-	document.getElementById("item").value="";
-	document.getElementById("precio").value="";
- }
- function eliminarRegistro(){
-	$(document).one('click','button[type="button"]', function(event){
-		let id=this.id;
-		var lista=[];
-		$("#listaProductos").each(function(){
-			var celdas=$(this).find('tr.Reg_'+id);
-			celdas.each(function(){
-				var registro=$(this).find('span.mid');
-				registro.each(function(){
-					lista.push($(this).html())
-				});
-			});
-		});
+function limpiar() {
+    document.getElementById("item").value = "";
+    document.getElementById("precio").value = "";
+}
+function eliminarRegistro() {
+    $(document).one('click', 'button[type="button"]', function (event) {
+        let id = this.id;
+        var lista = [];
+        $("#listaProductos").each(function () {
+            var celdas = $(this).find('tr.Reg_' + id);
+            celdas.each(function () {
+                var registro = $(this).find('span.mid');
+                registro.each(function () {
+                    lista.push($(this).html())
+                });
+            });
+        });
+        nuevoId = lista[0].substr(1);
+        //alert(nuevoId);
+        db.transaction(function (transaction) {
+            var sql = "borrar=" + nuevoId + ";"
+            transaction.executeSql(sql, undefined, function () {
+                alert("Registro borrado satisfactoriamente, Por favor actualice la tabla")
+            }, function (transaction, err) {
+                alert(err.message);
+            })
+        })
+    });
+}
